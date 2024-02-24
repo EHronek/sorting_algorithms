050fd26f89e2ad759ba12af44e276db8a85fdcae
EHronek <emmanuelhronek@gmail.com>
2024-02-24 13:21:04 +0000

    1. Insertion sort quick betty fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,33 +4,33 @@
 /**
   * insertion_sort_list - sorts a doubly linked list of integers in
   *    in ascending order using insertion sort
-  * list: the list to sort its node
+  * @list: the list to sort its node
   * Return: nothing
   */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-	    return;
-    current = (*list)->next;
-    while (current != NULL)
-    {
-	    temp = current->next;
+	listint_t *current, *temp;
 
-	    while (current->prev != NULL && current->n < current->prev->n)
-	    {
-		    current->prev->next = current->next;
-		    if (current->next != NULL)
-			    current->next->prev = current->prev;
-		    current->next = current->prev;
-		    current->prev = current->next->prev;
-		    current->next->prev = current;
-		    if (current->prev != NULL)
-			    current->prev->next = current;
-		    else
-			    *list = current;
-		    print_list(*list);
-	    }
-	    current = temp;
-    }
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current->next;
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+			if (current->prev != NULL)
+				current->prev->next = current;
+			else
+				*list = current;
+			print_list(*list);
+		}
+		current = temp;
+	}
 }
